5bd45223971928d6e88c5377f091d7673093ac84
Reham Hamdi <111997040+rehamhamdi@users.noreply.github.com>
2024-07-15 16:01:46 +0300

    Update

--- Create A New String Problem.java
+++ Create A New String Problem.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class problems {
-
+    
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String S = input.next();
